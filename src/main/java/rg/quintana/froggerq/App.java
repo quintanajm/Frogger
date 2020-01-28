0fb2467a9969c675eb6a99a263805f44990e9331
josem <josem@LAPTOP-6NEF8T4E>
2020-01-28 14:51:42 +0100

    movimiento

--- src/main/java/rg/quintana/froggerq/App.java
+++ src/main/java/rg/quintana/froggerq/App.java
@@ -1,14 +1,17 @@
 package rg.quintana.froggerq;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  * JavaFX App
@@ -18,7 +21,10 @@ public class App extends Application {
     final short SCENE_WIDTH = 648;
     int ranaHeight = 82;
     int ranaWidht = 101;
-    int ranaY = 715;
+    int ranaY = 700;
+    byte ranaDirection = 0;
+    byte ranaCurrentSpeed = 4;
+    short pasos = 0;
     
     @Override
     public void start(Stage stage) {
@@ -32,16 +38,56 @@ public class App extends Application {
         Image image1 = new Image(getClass().getResourceAsStream("/images/carreterarana.png")); 
         ImageView imageView1 = new ImageView(image1);
         root.getChildren().add(imageView1);
-        Image rana = new Image (getClass().getResourceAsStream("/images/ranilla.png"));
+        Image rana = new Image (getClass().getResourceAsStream("/images/ranaa.png"));
         ImageView imageView2 = new ImageView(rana);
         root.getChildren().add(imageView2);
-        imageView2.setX ((SCENE_WIDTH - ranaHeight )/2);
+        imageView2.setX ((SCENE_WIDTH - ranaWidht )/2);
         imageView2.setY(ranaY);
-    }
     
-
+        
+        
+    scene.setOnKeyPressed(new EventHandler <KeyEvent> (){
+            public void handle (final KeyEvent keyEvent){
+                switch (keyEvent.getCode()) {
+                    case UP:
+                        ranaDirection = -1;
+                    break;
+                        
+                    case DOWN:
+                        ranaDirection = 1;
+                    break;  
+                    
+                }    
+            }
+    });
+    Timeline timeline = new Timeline(
+            // 0.017 ~= 60 FPS
+            new KeyFrame(Duration.seconds(0.017), new EventHandler<ActionEvent>() {
+                public void handle(ActionEvent ae) {
+                    imageView2.setY(ranaY);
+                    ranaY += ranaCurrentSpeed * ranaDirection;
+                    if(ranaY <= 0 || ranaY >= SCENE_HEIGHT-ranaHeight) {
+                        ranaDirection = 0;
+                        pasos++;
+                    }
+                     if(ranaY <= 0) {
+                        ranaDirection = 0;
+                        ranaY = 0;
+                         System.out.println(pasos);
+                    }else if (ranaY >= SCENE_HEIGHT-ranaHeight) {
+                        ranaDirection = 0;
+                        ranaY = (short)(SCENE_HEIGHT-ranaHeight);
+                        pasos++;
+                    }
+                }
+            })                
+        );
+            timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();   
+    }
     public static void main(String[] args) {
         launch();
     }
 
 }
+        
