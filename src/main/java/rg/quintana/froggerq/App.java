167d9378e201269afa76892922bbe8743ef52979
josem <josem@LAPTOP-6NEF8T4E>
2020-02-04 14:57:05 +0100

    asignacion de grupos,a falta de colision

--- src/main/java/rg/quintana/froggerq/App.java
+++ src/main/java/rg/quintana/froggerq/App.java
@@ -1,15 +1,18 @@
 package rg.quintana.froggerq;
-
+//                                                    EL BUENO
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -19,8 +22,8 @@ import javafx.util.Duration;
 public class App extends Application {
     final short SCENE_HEIGHT = 800;
     final short SCENE_WIDTH = 648;
-    int ranaHeight = 82;
-    int ranaWidht = 101;
+    int ranaHeight = 108;
+    int ranaWidht = 128;
     int ranaY = 680;
     int ranaX = ((SCENE_WIDTH - ranaWidht )/2);
     byte ranaDirectionY = 0;
@@ -28,6 +31,9 @@ public class App extends Application {
     byte ranaCurrentSpeed = 4;
     short pasosY= 0;
     short pasosX= 0;
+    byte cochesCurrentSpeed = 4;
+    int imageViewCocheVerdeX = ((SCENE_WIDTH - ranaWidht )/2);
+    byte cocheDirectionX = -1;
     
     @Override
     public void start(Stage stage) {
@@ -37,6 +43,11 @@ public class App extends Application {
         stage.show();
         
         
+        short cocheverdeHeight = 50;
+        short cocheverdePosY = (short) ((SCENE_HEIGHT - cocheverdeHeight)/2);
+        byte cocheverdeCurrentSpeed = 2;
+        byte cocheverdeDirection = 0;
+        
 //      Imagen carretera
         Image image1 = new Image(getClass().getResourceAsStream("/images/carreterarana.png")); 
         ImageView imageView1 = new ImageView(image1);
@@ -45,19 +56,50 @@ public class App extends Application {
         
 //      Imagen rana
         Image rana = new Image (getClass().getResourceAsStream("/images/ranaa.png"));
-        ImageView imageView2 = new ImageView(rana);
-        root.getChildren().add(imageView2);
-        imageView2.setX ((SCENE_WIDTH - ranaWidht )/2);
-        imageView2.setY(ranaY);
+        ImageView imageViewRana = new ImageView(rana);
         
-//      Imagen coches descargados
+//      root.getChildren().add(imageView2);
+//        imageViewRana.setX (ranaX);
+//        imageViewRana.setY(ranaY);
+        
+//      Rectángulo rana
+        Rectangle rectrana = new Rectangle();
+        rectrana.setWidth(ranaWidht);
+        rectrana.setHeight(ranaHeight);
+//        rectrana.setX(ranaX);
+//        rectrana.setY(ranaY);
+        //root.getChildren().add(rectrana);
+        rectrana.setFill(Color.TRANSPARENT);
+        
+        Group grouprana = new Group ();
+        grouprana.getChildren().add(imageViewRana);
+        grouprana.getChildren().add(rectrana);  
+        root.getChildren().add(grouprana);
+        
+        
+        
+//      Imagen cocheverde
         Image cocheverde = new Image (getClass().getResourceAsStream("/images/cocheverde.png"));
-        ImageView imageView3 = new ImageView(cocheverde);
-        root.getChildren().add(imageView3);
-        imageView3.setX ((SCENE_WIDTH - ranaWidht )/2);
-        imageView3.setY(193);
+        ImageView imageViewCocheVerde = new ImageView(cocheverde);
+//        root.getChildren().add(imageViewCocheVerde);
+//        imageViewCocheVerde.setX ((SCENE_WIDTH - ranaWidht )/2);
+        imageViewCocheVerde.setY(193);
         
 //      Rectángulo cocheverde
+        Rectangle rectcocheverde = new Rectangle();
+        rectcocheverde.setWidth(185);
+        rectcocheverde.setHeight(91);
+//        rectcocheverde.setX((SCENE_WIDTH - ranaWidht )/2);
+        rectcocheverde.setY(193);
+//        root.getChildren().add(rectcocheverde);
+        rectcocheverde.setFill(Color.TRANSPARENT);
+        
+        Group groupcocheverde = new Group ();
+        groupcocheverde.getChildren().add(imageViewCocheVerde);
+        groupcocheverde.getChildren().add(rectcocheverde);  
+        root.getChildren().add(groupcocheverde);
+        
+        
       
 //        Image cochearcoiris = new Image (getClass().getResourceAsStream("/images/cochearcoiris.png"));
 //        ImageView imageView4 = new ImageView(cochearcoiris);
@@ -98,7 +140,7 @@ public class App extends Application {
             new KeyFrame(Duration.seconds(0.017), new EventHandler<ActionEvent>() {
                 public void handle(ActionEvent ae) {
                     
-                    imageView2.setY(ranaY);
+                    grouprana.setLayoutY(ranaY);
                     ranaY += ranaCurrentSpeed * ranaDirectionY;
 //                  Si se mueve en Y 
                     if (ranaDirectionY != 0){
@@ -124,7 +166,7 @@ public class App extends Application {
                     }
                     
 //                  La X de la rana
-                    imageView2.setX(ranaX);
+                    grouprana.setLayoutX(ranaX);
                     ranaX += ranaCurrentSpeed * ranaDirectionX;
                     
                     if (ranaDirectionX != 0){
@@ -145,7 +187,16 @@ public class App extends Application {
                         ranaDirectionX = 0;
                         pasosX = 0;
                     }
+                     
+                    groupcocheverde.setLayoutX(imageViewCocheVerdeX);
+                    imageViewCocheVerdeX +=cochesCurrentSpeed * cocheDirectionX;
+                    if(imageViewCocheVerdeX>= 510){
+                        cocheDirectionX=-1;
+                    } else if (imageViewCocheVerdeX<=0){
+                        cocheDirectionX= +1;
+                    } 
                 }
+                
             })  
     );
     timeline.setCycleCount(Timeline.INDEFINITE);
